7ccf7d0d25ee577a035b7dbfcf3405f0cd6ca512
Nick <nickdem559@inbox.ru>
2022-08-10 14:07:05 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-// import './App.css';
+import {
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+
+import Main from "./pages/Main/";
+import Other from "./pages/Other";
 
 function App() {
- 
+  return (
+    <Routes>
+      <Route path="/" exact element={<Main />} />
+      <Route path="/other" element={<Other />} />
+      <Route
+        path="*"
+        element={<Navigate to="/" replace />}
+    />
+    </Routes>
+  )
 }
 
 export default App;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,9 @@
+ function Main() {
+    return (
+        <div>
+          <h2>Home</h2>
+        </div>
+      );
+};
+
+export default Main
